644040acdd37731458c9c9a63e628520d5ae7094
《归·原》 <73215267+gcsaber@users.noreply.github.com>
2021-01-20 18:45:44 +0800

    同步课程
    同步课程进度

---
+++ Project1/Project1/37一维数组案例.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	//int arr[] = { 300,350,200,400,250 };
+	//int i = 0;
+	//int j = 0;
+	//cout << "请输入您要查询的小猪的编号" << endl;
+	//cin >> j ;
+	//i = j - 1;
+	//cout << "第" << j << "号小猪的重量为：" << arr[i] << endl;
+	//int max = 0;
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	if (arr[i] > max)
+	//	{
+	//		max = arr[i];
+	//	}
+	//}
+
+	//cout << "体重最重的小猪为" << max << "公斤" << endl;
+
+
+	system("pause");
+
+	return 0;
+}
