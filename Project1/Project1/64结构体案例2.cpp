b33ac1285d24c6ac4fc91a3b82d58e33a350545d
《归·原》 <73215267+gcsaber@users.noreply.github.com>
2021-02-01 15:25:37 +0800

    同步课程
    第一章内容全部完毕上传，cpp基础学习部分告一段路额

--- Project1/Project1/64结构体案例2.cpp
+++ Project1/Project1/64结构体案例2.cpp
@@ -1,1 +1,70 @@
 #include<iostream>
+using namespace std;
+
+//1、设计英雄结构体
+struct Hero
+{
+	//姓名
+	string name;
+	//年龄
+	int age;
+	//性别
+	string sex;
+};
+//冒泡排序
+void bubblesort(struct Hero heroArray[], int len)
+{
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = 0; j < len - i - 1; j++)//此处错误“j<j-i-1”，弱智错误少犯
+		{
+			if (heroArray[j].age > heroArray[j + 1].age)
+			{
+				struct Hero temp = heroArray[j];//对比元素是age交换的是数组所有元素，注意不要交换错
+				heroArray[j] = heroArray[j + 1];
+				heroArray[j + 1] = temp;
+			}
+		}
+	}
+} 
+
+//打印排序后的数组中的信息
+void printHero(struct Hero heroArray[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << "姓名：" << heroArray[i].name << " 年龄：" << heroArray[i].age << " 性别：" << heroArray[i].sex << endl;
+	}
+}
+
+int main()
+{
+
+	//2、创建数组存放五名英雄
+	struct Hero heroArray[5] =
+	{
+		{"刘备",23,"男"},
+		{"关羽",22,"男"},
+		{"张飞",20,"男"},
+		{"赵云",21,"男"},
+		{"貂蝉",19,"女"}
+	};
+
+	int len = sizeof(heroArray) / sizeof(heroArray[0]);
+	//for (int i = 0; i < len; i++)
+	//{
+	//	cout << "姓名：" << heroArray[i].name << " 年龄：" << heroArray[i].age << " 性别：" << heroArray[i].sex << endl;
+	//}
+
+	//3、对数组进行排序，按照年龄进行升序排列
+	bubblesort(heroArray, len);
+
+
+	//4、将排序后的结果打印输出
+	printHero(heroArray, len);
+
+
+	system("pause");
+
+	return 0;
+}
