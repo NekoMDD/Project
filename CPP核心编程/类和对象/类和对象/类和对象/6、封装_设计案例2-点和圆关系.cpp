5b1c1b4024cbdb52cef85623fd7b73dd767f76ba
《归·原》 <gcsaber@vip.qq.com>
2021-03-05 19:30:24 +0800

    同步课程
    同步课程进度

--- CPP核心编程/类和对象/类和对象/类和对象/6、封装_设计案例2-点和圆关系.cpp
+++ CPP核心编程/类和对象/类和对象/类和对象/6、封装_设计案例2-点和圆关系.cpp
@@ -1,64 +1,66 @@
 #include<iostream>
 using namespace std;
+#include"circle.h"
+#include"point.h"
 
 //点和圆的关系案例
 
 //点类
-class Point
-{
-public:
-	//设置X
-	void setX(int x)
-	{
-		m_X = x;
-	}
-	//获取X
-	int getX()
-	{
-		return m_X;
-	}
-	//设置Y
-	void setY(int y)
-	{
-		m_Y = y;
-	}
-	//获取Y
-	int getY()
-	{
-		return m_Y;
-	}
-private:
-	int m_X;
-	int m_Y;
-};
+//class Point
+//{
+//public:
+//	//设置X
+//	void setX(int x)
+//	{
+//		m_X = x;
+//	}
+//	//获取X
+//	int getX()
+//	{
+//		return m_X;
+//	}
+//	//设置Y
+//	void setY(int y)
+//	{
+//		m_Y = y;
+//	}
+//	//获取Y
+//	int getY()
+//	{
+//		return m_Y;
+//	}
+//private:
+//	int m_X;
+//	int m_Y;
+//};
 //创建圆类
-class Circle
-{
-public:
-	//设置半径
-	void setR(int r)
-	{
-		m_R = r;
-	}
-	//获取半径
-	int getR()
-	{
-		return m_R;
-	}
-	//设置圆心
-	void setCenter(Point center)
-	{
-		m_Center = center;
-	}
-	//获取圆心
-	Point getCenter()
-	{
-		return m_Center;
-	}
-private:
-	int m_R;//半径
-	Point m_Center;
-};
+//class Circle
+//{
+//public:
+//	//设置半径
+//	void setR(int r)
+//	{
+//		m_R = r;
+//	}
+//	//获取半径
+//	int getR()
+//	{
+//		return m_R;
+//	}
+//	//设置圆心
+//	void setCenter(Point center)
+//	{
+//		m_Center = center;
+//	}
+//	//获取圆心
+//	Point getCenter()
+//	{
+//		return m_Center;
+//	}
+//private:
+//	int m_R;//半径
+//	Point m_Center;
+//};
 
 //判断点和园的关系
 void isInCircle(Circle& c, Point& p)
